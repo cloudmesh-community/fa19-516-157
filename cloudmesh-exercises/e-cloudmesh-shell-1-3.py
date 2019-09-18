d0659e8229bfc7dc4e6dc7fbb0d3581bcd711afe
Chenxu <wangchenxu1995@gmail.com>
2019-09-17 22:05:52 -0400

    shell-3 complete

--- cloudmesh-exercises/e-cloudmesh-shell-1-3.py
+++ cloudmesh-exercises/e-cloudmesh-shell-1-3.py
@@ -1,3 +1,28 @@
 # e.cloudmesh.shell.1 cmd5 comes with cms install
 # e.cloudmesh.shell.2 folder has been uploaded to github
 # e.cloudmesh.shell.3 working
+from __future__ import print_function
+from cloudmesh.shell.command import command
+from cloudmesh.shell.command import PluginCommand
+
+
+class ChenxuCommand(PluginCOmmand):
+
+    @command
+    def do_chenxu(self, args, arguments):
+        """
+          ::
+           Usage:
+                 chenxu -f FILE
+                 chenxu list
+           This command does some useful things.
+           Arguments:
+              File     a file name
+           Options:
+                 -f    specify the file
+        """
+
+        print(arguments)
+        if arguments.FILE:
+            print("You have used file: ", arguments.FILE)
+        return ""
